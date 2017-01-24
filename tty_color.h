3e1ece8d4961efb6aaa84c8b8c621a04ced4b28f
igsong <igsong@gnsd.co.kr>
2017-01-24 11:08:48 +0900

    [Issue]       : added header file for log [Repository]  : [Description] :

---
+++ tty_color.h
@@ -0,0 +1,24 @@
+#ifndef _LINUX_TTY_COLOR_H
+#define _LINUX_TTY_COLOR_H
+
+#define tty_color(color)   "\e[38;5;" #color "m"
+#define tty_clear()        "\e[0m"
+
+#define log(color, fmt, arg...) \
+	printf(tty_color(color) fmt tty_clear() "\n", ##arg);
+
+#define logd(fmt, arg...) log(39, fmt, ##arg);
+#define logi(fmt, arg...) log(190, fmt, ##arg);
+#define logw(fmt, arg...) log(208, fmt, ##arg);
+#define loge(fmt, arg...) log(135, fmt, ##arg);
+
+void tty_color_print()
+{
+    int i;
+    for (i = 0; i < 256; i++) {
+        printf("\e[38;5;%dm%d \e[0m" , i, i);
+    }
+    printf("\n");
+}
+
+#endif  /* _LINUX_TTY_COLOR_H */
